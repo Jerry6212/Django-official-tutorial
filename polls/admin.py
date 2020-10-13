7b108e41416958c1277796a80522615e65161113
Jerry Ploegert <jerryjploegert@gmail.com>
2020-10-13 00:43:22 -0500

    Made changes to admin page, now able to publish questions and choices

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Question
 
-admin.site.register(Question)
+from .models import Question, Choice
 
+
+# This changes around the order of fields on the admin page
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
