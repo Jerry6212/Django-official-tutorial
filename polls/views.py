a417d1b99123aaaee7d83c4f1e45cc695c2ae61f
Jerry Ploegert <jerryjploegert@gmail.com>
2020-10-13 00:25:31 -0500

    Made views generic, included unit test, made unplublished polls unavailable

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,33 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
+from datetime import timezone
+import datetime
+
+now = datetime.datetime.now()
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # return last 5 questions that are currently published
+        return Question.objects.filter(
+            pub_date__lte=now
+        ).order_by('-pub_date')[:5]
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+        # This gets rid of any questions that are not yet published
+        return Question.objects.filter(pub_date__lte=now)
 
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
 def vote(request, question_id):
