cc2e4ed98aa9f4410bf3afd8a0eaff5b68ded230
Jerry Ploegert <jerryjploegert@gmail.com>
2020-10-14 08:15:23 -0500

    Updated admin page
    Admin page now allows user to upload questons and multiple choices to each question, also added a filter and a seach bar to the admin page to find questions.

--- polls/admin.py
+++ polls/admin.py
@@ -3,13 +3,23 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-# This changes around the order of fields on the admin page
+# This changes around the order of fields on the admin page and allows admin to add question and multiple choices
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# Aligns the choice text and makes the tabular
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+
     ]
-
-
+    # Adds fields showing publish dates, if published recently, a filter, and a search bar
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
